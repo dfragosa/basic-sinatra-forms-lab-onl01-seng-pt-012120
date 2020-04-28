50c0b70b919baf3eea8b96deec7f1e4baa387aa2
Derric Fragosa <fderric@gmail.com>
2020-04-28 18:15:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,12 +19,8 @@ class App < Sinatra::Base
     erb :team
   end
   
-  error Document::NoFilesFoundError Document
-    status 404
-    @page = Page::BasicPage.new(title: 'Page Not Found =404')
-    erb :fourohfour
+  post '/status' do
+    erb :status
   end
-end
-
   
 end
